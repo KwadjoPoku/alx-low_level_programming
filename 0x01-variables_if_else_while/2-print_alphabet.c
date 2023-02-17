271d7b4cecf8c1816acf7ffc1df59a68310f9605
KwadjoPoku <e.opoku2018@gmail.com>
2023-02-17 15:57:09 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char g;
+
+	c = 'a';
+	while
+		(g <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
