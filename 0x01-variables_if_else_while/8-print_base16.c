64cd758ee8146427e2dfb911cce3e94eb07f886a
KwadjoPoku <e.opoku2018@gmail.com>
2023-02-17 17:19:35 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (win)
+ */
+
+int main(void)
+{
+	char n;
+
+	int f;
+
+	n = 'a';
+	f = 0;
+	while
+		(f < 10) {
+			putchar(f + '0');
+			f++;
+		}
+	while
+		(n <= 'f') {
+			putchar(n);
+			n++;
+		}
+	putchar('\n');
+	return (0);
+}
