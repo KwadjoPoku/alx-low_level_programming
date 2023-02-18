fbe3f020153b5b682057f1e1a240b5667b4e987a
KwadjoPoku <e.opoku2018@gmail.com>
2023-02-17 16:28:59 -0800

    q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (wins)
+ */
+
+int main(void)
+{
+	char u;
+
+	u = 'a';
+	while
+		(u <= 'z') {
+			if ((u != 'q' || u != 'e') && u <= 'z')
+				putchar(u);
+			u++;
+		}
+	putchar('\n');
+	return (0);
+}
