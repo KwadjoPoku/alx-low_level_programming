890cacac06c7608a7d1748d80443bc01e003971c
KwadjoPoku <e.opoku2018@gmail.com>
2023-02-17 16:13:20 -0800

    A to Z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char j;
+
+	char k;
+
+	j = 'a';
+	k = 'A';
+	while
+		(j <= 'z') {
+			putchar(j);
+			j++;
+		}
+	while
+		(k <= 'Z') {
+			putchar(k);
+			k++;
+		}
+	putchar('\n');
+	return (0);
+}
