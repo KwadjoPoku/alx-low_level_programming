2dfe27d01694b181675c108133122667656b0556
KwadjoPoku <e.opoku2018@gmail.com>
2023-02-17 16:53:52 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int t;
+
+	t = 0;
+	while
+		(t < 10) {
+			putchar(t + '0');
+			t++;
+		}
+	putchar('\n');
+	return (0);
+}
