deb9c1207ab0773b41b5311c68057a215e949f2c
KwadjoPoku <e.opoku2018@gmail.com>
2023-02-17 16:42:36 -0800

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (winner)
+ */
+
+int main(void)
+{
+	int p;
+
+	for (p = 0; p < 10; p++)
+		printf("%d", p);
+	putchar('\n');
+	return (0);
+}
