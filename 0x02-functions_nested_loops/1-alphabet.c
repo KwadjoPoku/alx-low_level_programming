22618d513b671fd8bfb6b391b87eeb3d6d9fea34
KwadjoPoku <e.opoku2018@gmail.com>
2023-02-21 14:54:26 -0800

    Nancy

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make alphabet
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char v;
+	for (v = 'a'; v <= 'z'; v++)
+	{
+	_putchar(v);
+	}
+	_putchar('\n');
+}
