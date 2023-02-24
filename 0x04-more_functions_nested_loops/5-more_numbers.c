7a99c8fe2245446941a39f419c3165f7cfa90c9e
KwadjoPoku <e.opoku2018@gmail.com>
2023-02-23 17:19:51 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - Print 10 times the numbers 0 to 14
+ * Return: 10 times the numbers 0 to 14
+ */
+
+void more_numbers(void)
+{
+	int g, h;
+
+	for (g = 0; g < 10; g++)
+	{
+	for (h = 0; h <= 14; h++)
+	{
+	if (h > 9)
+	{
+	_putchar((h / 10) + '0');
+	}
+	_putchar((h % 10) + '0');
+	}
+	_putchar('\n');
+	}
+}
